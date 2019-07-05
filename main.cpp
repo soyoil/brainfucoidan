69d2cf025aa27b6c30ed66abe5c8f8002514b5e4
soyoil <kappamaki8801@gmail.com>
2019-07-05 17:52:20 +0900

    fix:大幅なコード書き換え

--- main.cpp
+++ main.cpp
@@ -1,61 +1,110 @@
 #include <iostream>
 #include <fstream>
-#define NUM 30000
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#define MEMORY 30000
 
-int main(int argc,char *argv[]){
-  FILE *f;
-  char c[NUM];
-  unsigned char b[NUM];
-  int cp=0,cl=0,bp=0,l=0;
-  int i;
-  if(argc != 2 || (f=fopen(argv[1],"r")) == NULL){
-  std::cout << "error!" << std::endl;
-  return -1;
+int main(int argc, char *argv[]){
+
+  // 引数が2ではない（ファイルが指定されていなかったり，二個以上指定されている）時にエラーを返す
+  if(argc != 2){
+    std::cerr << "ファイルが指定されていないか，２つ以上指定されています" << std::endl;
+    return -1;
   }
-  for(cl = 0; (c[cl]=fgetc(f)) != EOF && cl < NUM; cl++);
-  for(i = 0; i < NUM; i++) b[i] = 0;
-  for(; cp < cl; cp++){
-    switch(c[cp]){
-      case '>':
-        bp++;
+
+  // ifs ステートメントでファイルを開く
+  std::ifstream ifs(argv[1]);
+
+  // fail メゾットでファイルの存在を調べ，なければエラーを返す
+  if(ifs.fail()){
+    std::cerr << "そのようなファイルはありません" << std::endl;
+    return -1;
+  }
+
+  std::string src = "";
+  std::string temp;
+
+  // getline メゾットで一行ずつ読み込み，append関数で末尾に文字列を付け足して全体を1行に直す
+  while(getline(ifs, temp)) src.append(temp);
+
+  // vectorステートメントでunsigned char型が入る30000個の配列を作り，0で初期化する．
+  std::vector<unsigned char> mem(MEMORY, 0);
+
+  // ポインタの位置として変数posを宣言する
+  unsigned int pos = 0;
+
+  // size 関数で文字列のサイズを取得し，1文字づつ処理を進める
+  for(int i = 0; i < (int)src.size(); i++){
+    switch(src[i]){
+
+      case '>': // ポインタのインクリメント
+        pos++;
+        if(pos == mem.size()) mem.resize(mem.size() * 2, 0); // メモリが足りなければ拡張する
         break;
-      case '<':
-        bp--;
+
+      case '<': // ポインタのデクリメント
+        if(pos == 0){
+          std::cerr << "ポインタの値を負にすることはできません" << std::endl;
+          return -1;
+        }
+        pos--;
         break;
-      case '+':
-        b[bp]++;
+
+      case '+': // メモリのインクリメント
+        mem[pos]++;
         break;
-      case '-':
-        b[bp]--;
+
+      case '-': // メモリのデクリメント 
+        mem[pos]--;
         break;
-      case '.':
-        putchar(b[bp]);
+
+      case '.': // メモリの値が示す文字列の出力
+        putchar(mem[pos]);
         break;
-      case ',':
-        b[bp] = getchar();
+
+      case ',': // 入力された値をメモリに格納
+        mem[pos] = getchar();
         break;
-      case '[':
-        if(b[bp] == 0){
-          for(cp++; l > 0 || c[cp] != ']'; cp++){
-            if(c[cp] == '[')l ++;
-            if(c[cp ]== ']') l--;
+
+      case '[': // ポインタの値が0なら，対応する]の直後にジャンプする
+        if(mem[pos] == 0){
+          int cnt = 1;
+          while(cnt != 0){
+            i++;
+            // 探索が終端まで達してしまったらエラーを返す
+            if(src.size()){
+              std::cerr << "対応する']'がありません" << std::endl;
+              return -1;
+            }
+            // 入れ子の[]を考慮している
+            if(src[i] == '[') cnt++;
+            else if(src[i] == ']') cnt--;
           }
         }
         break;
-      case ']':
-        if(b[bp]!=0){
-          for(cp--; l>0 || c[cp] != '['; cp--){
-            if(c[cp] == '[') l--;
-            if(c[cp] == ']') l++;
-            cp--;
+
+      case ']': // ポインタの値が0でないなら，対応する[の直後にジャンプする
+        if(mem[pos] != 0){
+          int cnt = 1;
+          while(cnt != 0){
+            i--;
+            // 探索が先頭まで達してしまったらエラーを返す
+            if(i < 0){
+              std::cerr << "対応する'['がありません" << std::endl;
+              return -1;
+            }
+            if(src[i] == ']') cnt++;
+            else if(src[i] == '[') cnt--;
           }
         }
         break;
+
       default:
         break;
     }
   }
-  fclose(f);
-  std::cout << "execution end" << std::endl;
+  std::cout << std::endl << "処理が終了しました" << std::endl;
   return 0;
 }
