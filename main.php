f8481d0efcdf7f0303bdf72b5d86972e36c73911
soyoil <kappamaki8801@gmail.com>
2019-07-05 18:09:28 +0900

    fix:修正

--- main.php
+++ main.php
@@ -1,27 +1,7 @@
 <?php
 
-class Brainfuck{
-
-  private $code = null;
-  private $codePointer = 0;
-  private $cell = array();
-  private $pointer = 0;
-  private $input = null;
-  private $inputPointer = 0;
-  private $buffer = array();
-  private $output = '';
-  private $wrap = true;
-
-  public function __construct($code, $input = null, $wrap = null){
-    $this->code = $code;
-    $this->input = ($input) ? $input : null;
-    $this->wrap = (boolean) $wrap;
-  }
-
-  public function run($return = false){
-    while ($this->codePointer < strlen($this->code)) {
-      
-    }
-  }
-
+if($argc != 2){
+  echo "ファイルが指定されていないか，２つ以上指定されています";
+  exit(1);
 }
+
